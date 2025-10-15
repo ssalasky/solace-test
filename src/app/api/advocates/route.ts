68f246b0515081aceb7ddc5adfc3fa195d7473d6
ssalasky <samsalasky@gmail.com>
2025-10-15 17:29:49 -0600

    separation: Separated functionality by component and best practices

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,24 @@
-import db from "../../../db";
-import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import {advocateData} from "@/db/seed/advocates";
 
-export async function GET() {
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  let searchTerm = url.searchParams.get("searchTerm")?.toLowerCase() || "";
+  if (searchTerm) {
+    searchTerm = searchTerm.toLowerCase();
+  }
   // Uncomment this line to use a database
   // const data = await db.select().from(advocates);
 
-  const data = advocateData;
+  const data = searchTerm ? advocateData.filter((advocate) => {
+    return (
+      advocate.firstName.includes(searchTerm) ||
+      advocate.lastName.includes(searchTerm) ||
+      advocate.city.includes(searchTerm) ||
+      advocate.degree.includes(searchTerm) ||
+      advocate.specialties.includes(searchTerm) ||
+      advocate.yearsOfExperience.toString().includes(searchTerm)
+    );
+  }) : advocateData;
 
   return Response.json({ data });
 }
